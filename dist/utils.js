2ab2aca9b8e9b94dcec57f1d6cc6d7883ae367fc
Pau Pamos <paupamos@gmail.com>
2025-11-14 11:47:30 +0100

    Estandaritzant utils

--- dist/utils.js
+++ dist/utils.js
@@ -1,63 +1,136 @@
-export function Vector2(x,y){
-	const obj = {};
-	
-	obj.x = x;
-	obj.y = y;
-	
-	obj.ProjX = function(){
-		return(Vector2(x,0));
+class Vector
+{
+	constructor(x, y)
+	{
+		this.x = x;
+		this.y = y;		
 	}
-	obj.ProjY = function(){
-		return(Vector2(0,y));
-	}
-	obj.Scale = function(s){
-		return(Vector2(obj.x * s, obj.y * s));
-	}
-	obj.Sum = function(other){
-		return(Vector2(other.x + obj.x, other.y + obj.y))
-	}
-	obj.Sub = function(other){
-		return(Vector2(other.x - obj.x, other.y - obj.y))
-	}
-	obj.Magnitude = function(){
-		return(Math.sqrt(x*x + y*y));
-	}
-	obj.Normalize = function(){
-		let m = obj.Magnitude();
-		return(Vector2(obj.x/m,obj.y/m));
-	}
-	obj.Randomize = function(){
-		let ret = Vector2(Math.random(),Math.random());
-		return(ret.Normalize());
-	}
-	obj.DotProduct = function(other){
-		return(obj.x * other.x + obj.y * other.y);
-	}
-	obj.Neg = function(){
-		return(Vector2(-obj.x,-obj.y));
-	}
-	obj.Rotate = function(deg){
-		return(Vector2(obj.x * Math.cos(deg) - y * Math.sin(deg), obj.x * Math.sin(deg) + obj.y * Math.cos(deg)));
-	}
-	obj.Angle = function(other){
-		return(Math.acos(obj.DotProduct(other)/(obj.Magnitude() * other.Magnitude())));
-	}
-	obj.OrthoX = function(){
-		return(Vector2(-obj.y, obj.x));
-	}
-	obj.OrthoY = function(other){
-		return(Vector2(obj.y, -obj.x));
+
+	rotate(deg)
+	{
+		const ox = this.x;
+		const oy = this.y;
+		this.x = Math.cos(deg) * ox - Math.sin(deg) * oy
+		this.y = Math.sin(deg) * ox + Math.cos(deg) * oy 
 	}
 	
-	return(obj);
+	translate(x, y)
+	{
+		this.x += x;
+		this.y += y;
+	}
+	
+	scale(dis)
+	{
+		this.x = this.x * dis;
+		this.y = this.y * dis;
+	}
+	
+	uscale(x, y)
+	{
+		this.x = this.x * x;
+		this.y = this.y * y;
+	}
+	
+	sum(vec)
+	{
+		this.x += vec.x;
+		this.y += vec.y;
+	}
+	
+	rotatePivot(deg, pos)
+	{
+		this.translate(-pos.x, -pos.y);
+		this.rotate(deg);
+		this.translate(pos.x, pos.y);
+	}
+	
+	magnitude()
+	{
+		if( this.x == 0 && this.y == 0)
+		{
+			return(1);
+		}
+		return(Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2)))
+	}
+
+	distance(vec)
+	{
+		let aux = vec.clone();
+		
+		aux.scale(-1);
+		aux.sum(this);
+		
+		return(aux.magnitude());
+	}
+	
+	modDistance(vec,xbound, ybound)
+	{
+		let auxhl = vec.clone();
+		let auxvl = vec.clone();
+		let auxbl = vec.clone();
+		let auxhu = vec.clone();
+		let auxvu = vec.clone();
+		let auxbu = vec.clone();
+		
+		let mhl = 0;
+		let mvl = 0;
+		let mbl = 0;
+		let mhu = 0;
+		let mvu = 0;
+		let mbu = 0;
+		
+		let mag = 0;
+		
+		auxhl.translate(-xbound, 0);
+		auxvl.translate(0, -ybound);
+		auxbl.translate(-xbound, -ybound);
+		auxhu.translate(+xbound, 0);
+		auxvu.translate(0, +ybound);
+		auxbu.translate(+xbound, +ybound);
+		
+		auxhl.scale(-1);
+		auxvl.scale(-1);
+		auxbl.scale(-1);
+		auxhu.scale(-1);
+		auxvu.scale(-1);
+		auxbu.scale(-1);
+		
+		auxhl.sum(this);
+		auxvl.sum(this);
+		auxbl.sum(this);
+		auxhu.sum(this);
+		auxvu.sum(this);
+		auxbu.sum(this);
+		
+		mhl = auxhl.magnitude();
+		mvl = auxvl.magnitude();
+		mbl = auxbl.magnitude();
+		mhu = auxhu.magnitude();
+		mvu = auxvu.magnitude();
+		mbu = auxbu.magnitude();
+		mag = this.distance(vec);
+		
+		return Math.min(Math.min(Math.min(Math.min(Math.min(Math.min(mag, mhl), mhu), mvl), mvu), mbl), mbu);
+	}
+
+	normalize()
+	{
+		const mag = this.magnitude();
+		this.x = this.x/mag;
+		this.y = this.y/mag;
+	}
+	
+	ortho()
+	{
+		let aux = this.x;
+		this.x = - this.y;
+		this.y = aux;
+	}
+	
+	clone()
+	{
+		return(new Vector(this.x, this.y));
+	}
 }
 
-export function Clamp(val, min, max){
-	if (val>max){
-		return(max);
-	}else if (val < min){
-		return(min);
-	}else{
-		return(val);
-	}
-}
